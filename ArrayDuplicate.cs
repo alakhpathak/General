c91f34722d9dc35c74aa9354a71fa4945bb74ffa
Alakh Niranjan Pathak <alakh.pathak@mediakind.com>
2020-05-08 22:44:39 +0530

    some general programming

---
+++ ArrayDuplicate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ArrayDuplicate
+{
+    class Program
+    {
+        static void FindDuplicate(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[Math.Abs(array[i])] >= 0)
+                {
+                    array[Math.Abs(array[i])] = -array[Math.Abs(array[i])];
+                }
+                else
+                {
+                    Console.WriteLine("Duplicate at "+i+" "+Math.Abs(array[i]) + " ");
+                }
+                PrintArray(array);
+            }
+            Console.ReadLine();
+        }
+
+        static void PrintArray(int[] a)
+        {
+            Console.WriteLine("********");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + ",");
+            }
+            Console.WriteLine("********");
+        }
+
+        // Driver program  
+        public static void Main()
+        {
+            int[] array = { 3, 2, 1, 6, 4, 3, 4 };
+            FindDuplicate(array);
+        }
+    }
+}
